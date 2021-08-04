3112fbf20cec1b08305581b31dc2e692ba3e32e7
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-08-04 11:58:52 -0500

    Done.

---
+++ oldIndex.js
@@ -0,0 +1,5 @@
+// // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     const changeText = document.getElementById("text");
+//     changeText.innerHTML = 'This is really cool!';
+// });
